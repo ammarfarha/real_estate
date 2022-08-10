e272596fd4946ca7df9d3c2c70be9cf64d1736aa
Amma Farha <ammarfarha@gmail.com>
2022-08-10 13:30:46 -0700

    make chanmges to accept multilanguages

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +125,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGES_CODE = 'en'
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('ar', _('French')),
+    ('fr', _('عربي')),
+)
